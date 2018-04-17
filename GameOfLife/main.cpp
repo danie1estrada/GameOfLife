2e8ea70fcf4773aa8ad9d6a8cf6d7c04a64ce2f4
Daniel Estrada <isc.daniel.estrada@gmail.com>
2018-04-16 19:18:47 -0500

    Stable release

--- GameOfLife/main.cpp
+++ GameOfLife/main.cpp
@@ -1,22 +1,11 @@
-/**
- * @file main.cpp
- * @version 2.0
- * @author Daniel Estrada
- * @title Main
- * @
- */
 #include "EventHandler.h"
 #include <thread>
-
-void Something(GameOfLife *game);
 
 int main(int argc, char *argv[]) {
 	
 	GameOfLife game(100, 190, 5);
 	EventHandler handler(&game);
 	SDL_Event event;
-
-	std::thread t(Something, &game);
 
 	while (game.IsRunning()) {
 		if (SDL_PollEvent(&event)) {
@@ -28,23 +17,4 @@ int main(int argc, char *argv[]) {
 	}
 
 	return 0;
-}
-
-void Something(GameOfLife *game) {
-	int answer, r, g, b;
-
-	while (true) {
-		std::cout << "Elija\n0. Color\n1. Fondo\nRespuesta: ";
-		std::cin >> answer;
-		std::cout << "Ingrese los valores: ";
-
-		if (answer == 0) {
-			std::cin >> r >> g >> b;
-			game->SetColor(r, g, b);
-		} else if (answer == 1) {
-			std::cin >> r >> g >> b;
-			game->SetBackgroundColor(r, g, b);
-		}
-		std::cout << std::endl;
-	}
 }
